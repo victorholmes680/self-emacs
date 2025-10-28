d826fd23060b91e3576ce04458960571ee3011e1
victorholmes <victorholmes680@gmail.com>
2025-10-28 15:32:38 +0800

    feat: add code completion

--- lisp/init-java.el
+++ lisp/init-java.el
@@ -1,7 +1,7 @@
 ;; init-java.el
-;; Java development configuration with Maven support
+;; Simplified Java development configuration
 
-;; Java Mode - built-in mode, no package needed
+;; Java Mode basic setup
 (add-to-list 'auto-mode-alist '("\\.java\\'" . java-mode))
 (add-hook 'java-mode-hook
           (lambda ()
@@ -9,161 +9,75 @@
                   tab-width 4
                   indent-tabs-mode nil)))
 
-;; Maven integration using compilation mode instead of non-existent maven package
-;; Alternative: You could use maven-mode if available, but it's not in MELPA
+;; Maven integration
 (setq maven-executable "/Users/wangzhixiong/Downloads/apache-maven-3.9.9/bin/mvn")
 
-;; Custom Maven functions for better integration
+;; Maven functions
 (defun my-java-mvn-compile ()
-  "Run Maven compile in project root."
+  "Run Maven compile."
   (interactive)
   (let ((root (locate-dominating-file default-directory "pom.xml")))
     (if root
         (progn
           (cd root)
           (compile "mvn compile"))
-      (message "No pom.xml found in project hierarchy"))))
+      (message "No pom.xml found"))))
 
 (defun my-java-mvn-test ()
-  "Run Maven test in project root."
+  "Run Maven test."
   (interactive)
   (let ((root (locate-dominating-file default-directory "pom.xml")))
     (if root
         (progn
           (cd root)
           (compile "mvn test"))
-      (message "No pom.xml found in project hierarchy"))))
+      (message "No pom.xml found"))))
 
-(defun my-java-mvn-package ()
-  "Run Maven package in project root."
-  (interactive)
-  (let ((root (locate-dominating-file default-directory "pom.xml")))
-    (if root
-        (progn
-          (cd root)
-          (compile "mvn package"))
-      (message "No pom.xml found in project hierarchy"))))
-
-(defun my-java-mvn-install ()
-  "Run Maven install in project root."
-  (interactive)
-  (let ((root (locate-dominating-file default-directory "pom.xml")))
-    (if root
-        (progn
-          (cd root)
-          (compile "mvn install"))
-      (message "No pom.xml found in project hierarchy"))))
-
-(defun my-java-mvn-clean ()
-  "Run Maven clean in project root."
-  (interactive)
-  (let ((root (locate-dominating-file default-directory "pom.xml")))
-    (if root
-        (progn
-          (cd root)
-          (compile "mvn clean"))
-      (message "No pom.xml found in project hierarchy"))))
-
-(defun my-java-mvn-custom-task (task)
-  "Run custom Maven TASK in project root."
-  (interactive (list (read-string "Maven task: " "mvn ")))
-  (let ((root (locate-dominating-file default-directory "pom.xml")))
-    (if root
-        (progn
-          (cd root)
-          (compile task))
-      (message "No pom.xml found in project hierarchy"))))
-
-;; Java LSP Server configuration (Eclipse JDT.LS)
+;; Java LSP Server configuration
 (use-package lsp-java
   :after lsp-mode
   :config
-  ;; Set Java home based on your environment
+  ;; Java home
   (setq lsp-java-java-path "/opt/homebrew/Cellar/openjdk/23.0.2/libexec/openjdk.jdk/Contents/Home/bin/java")
   (setq lsp-java-java-home "/opt/homebrew/Cellar/openjdk/23.0.2/libexec/openjdk.jdk/Contents/Home")
 
-  ;; Use Homebrew-installed JDT.LS
+  ;; JDT.LS location
   (setq lsp-java-server-install-dir "/opt/homebrew/Cellar/jdtls/1.47.0/libexec")
 
-  ;; Set Maven configuration
+  ;; Basic settings
   (setq lsp-java-maven-download-sources t
         lsp-java-maven-download-javadoc t
-        lsp-java-import-gradle-enabled t
-        lsp-java-import-maven-enabled t
         lsp-java-signature-help-enabled t
         lsp-java-completion-overwrite t
-        lsp-java-max-concurrent-builds 1
         lsp-java-workspace-dir (expand-file-name "~/repo/.lsp-java-workspace/")
         lsp-java-format-enabled t
-        lsp-java-format-comments-enabled t
         lsp-java-save-action-organize-imports t)
 
-  ;; Custom Maven settings for JDT.LS
-  (setq lsp-java-configuration-maven-user-settings
-        (expand-file-name "~/.m2/settings.xml"))
-
-  ;; Configure Maven behavior
-  (setq lsp-java-maven-prefer-online-resources nil
-        lsp-java-configuration-update-build-configuration t
-        lsp-java-java-home "/opt/homebrew/Cellar/openjdk/23.0.2/libexec/openjdk.jdk/Contents/Home")
-
-  ;; Lombok agent configuration
+  ;; Lombok support
   (setq lsp-java-vmargs
         (list
          "-noverify"
          "-Xmx1G"
          "-XX:+UseG1GC"
-         "-XX:+UseStringDeduplication"
          "-javaagent:/Users/wangzhixiong/.emacs.d/lombok.jar"
          "-Xbootclasspath/a:/Users/wangzhixiong/.emacs.d/lombok.jar"))
 
-  ;; Key bindings for Java LSP
-  (define-key java-mode-map (kbd "C-c j j") 'lsp-java-add-overridable-methods)
-  (define-key java-mode-map (kbd "C-c j t") 'lsp-java-generate-to-string)
-  (define-key java-mode-map (kbd "C-c j h") 'lsp-java-generate-hashcode-equals)
-  (define-key java-mode-map (kbd "C-c j c") 'lsp-java-generate-constructors)
-  (define-key java-mode-map (kbd "C-c j g") 'lsp-java-generate-getters-setters)
-  (define-key java-mode-map (kbd "C-c j d") 'lsp-java-generate-delegate)
+  ;; Java LSP key bindings
   (define-key java-mode-map (kbd "C-c j o") 'lsp-java-organize-imports)
   (define-key java-mode-map (kbd "C-c j f") 'lsp-java-format)
-  (define-key java-mode-map (kbd "C-c j r") 'lsp-java-rename-symbol))
-
-;; Java import organization
-(use-package importmagic
-  :commands importmagic-mode
-  :config
-  (setq importmagic-skip-compression t
-        importmagic-auto-import-behavior 'ask)
-  (add-hook 'java-mode-hook 'importmagic-mode))
+  (define-key java-mode-map (kbd "C-c j g") 'lsp-java-generate-getters-setters))
 
 ;; Auto-enable Java configuration
 (add-hook 'java-mode-hook
           (lambda ()
-            ;; Enable LSP for Java
-            (when (and (locate-dominating-file default-directory "pom.xml")
-                       (fboundp 'lsp-deferred))
-              (lsp-deferred))
-
-            ;; Set up key bindings for Maven commands
+            ;; Maven shortcuts
             (local-set-key (kbd "C-c C-c c") 'my-java-mvn-compile)
             (local-set-key (kbd "C-c C-c t") 'my-java-mvn-test)
-            (local-set-key (kbd "C-c C-c p") 'my-java-mvn-package)
-            (local-set-key (kbd "C-c C-c i") 'my-java-mvn-install)
-            (local-set-key (kbd "C-c C-c k") 'my-java-mvn-clean)
-            (local-set-key (kbd "C-c C-c r") 'my-java-mvn-custom-task)
 
-            ;; Java-specific settings
-            (setq c-basic-offset 4
-                  tab-width 4
-                  indent-tabs-mode nil)))
-
-;; DAP mode for debugging (optional)
-(use-package dap-mode
-  :after lsp-mode
-  :commands dap-debug
-  :config
-  ;; Java DAP configuration
-  (require 'dap-java)
-  (setq dap-auto-configure-features '(sessions locals breakpoints expressions tooltip)))
+            ;; LSP shortcuts
+            (local-set-key (kbd "C-c .") 'lsp-find-definition)
+            (local-set-key (kbd "C-c ,") 'lsp-find-references)
+            (local-set-key (kbd "C-c /") 'lsp-rename)
+            (local-set-key (kbd "C-c ;") 'lsp-execute-code-action)))
 
 (provide 'init-java)
--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,5 +1,5 @@
 ;; init-lsp.el
-;; Language Server Protocol configuration
+;; Simplified LSP configuration for Java development
 
 ;; LSP Mode
 (use-package lsp-mode
@@ -7,114 +7,74 @@
   (setq lsp-keymap-prefix "C-c l")
   :commands lsp
   :config
+  ;; Basic LSP settings
   (setq lsp-auto-guess-root t
         lsp-prefer-capf t
-        lsp-enable-file-watchers nil
-        lsp-enable-folding nil
-        lsp-enable-snippet nil
+        lsp-enable-file-watchers t
+        lsp-enable-snippet t
         lsp-enable-symbol-highlighting t
-        lsp-enable-on-type-formatting nil
-        lsp-signature-auto-activate nil
-        lsp-modeline-code-actions-enable nil
-        lsp-modeline-diagnostics-enable nil
-        lsp-headerline-breadcrumb-enable nil
-        lsp-semantic-tokens-enable nil
-        lsp-enable-indentation nil
-        lsp-enable-links nil)
+        lsp-signature-auto-activate t
+        lsp-signature-auto-activate-delay 0.2
+        lsp-modeline-code-actions-enable t
+        lsp-modeline-diagnostics-enable t
+        lsp-completion-enable t)
 
-  ;; Performance improvements
-  (setq read-process-output-max (* 1024 1024))  ; 1MB
-  (setq gc-cons-threshold 100000000)  ; 100MB
+  ;; Performance
+  (setq read-process-output-max (* 1024 1024))
+  (setq gc-cons-threshold 100000000)
 
   ;; Key bindings
-  (define-key lsp-mode-map (kbd "C-c l r r") 'lsp-rename)
-  (define-key lsp-mode-map (kbd "C-c l g g") 'lsp-find-definition)
-  (define-key lsp-mode-map (kbd "C-c l g r") 'lsp-find-references)
-  (define-key lsp-mode-map (kbd "C-c l a a") 'lsp-execute-code-action)
-  (define-key lsp-mode-map (kbd "C-c l f f") 'lsp-format-buffer)
-  (define-key lsp-mode-map (kbd "C-c l s s") 'lsp-workspace-symbol)
-  )
+  (define-key lsp-mode-map (kbd "C-c l r") 'lsp-rename)
+  (define-key lsp-mode-map (kbd "C-c l g") 'lsp-find-definition)
+  (define-key lsp-mode-map (kbd "C-c l f") 'lsp-format-buffer))
 
-;; LSP UI - Better UI for LSP
+;; LSP UI
 (use-package lsp-ui
   :commands lsp-ui-mode
   :config
   (setq lsp-ui-doc-enable t
         lsp-ui-doc-use-childframe t
         lsp-ui-doc-position 'top
-        lsp-ui-doc-include-signature t
-        lsp-ui-sideline-enable nil
-        lsp-ui-sideline-show-code-actions nil
-        lsp-ui-sideline-show-hover nil
-        lsp-ui-sideline-show-diagnostics nil
-        lsp-ui-sideline-ignore-duplicate t
-        lsp-ui-peek-enable t
-        lsp-ui-peek-list-width 60
-        lsp-ui-peek-peek-height 25)
-
-  ;; Key bindings for LSP UI
-  (define-key lsp-ui-mode-map (kbd "M-.") 'lsp-ui-peek-find-definitions)
-  (define-key lsp-ui-mode-map (kbd "M-,") 'lsp-ui-peek-find-references)
-  )
+        lsp-ui-doc-delay 0.2
+        lsp-ui-sideline-enable t
+        lsp-ui-sideline-show-hover t
+        lsp-ui-sideline-show-diagnostics t
+        lsp-ui-peek-enable t))
 
 ;; Company mode for completion
 (use-package company
   :commands company-mode
   :config
-  (setq company-idle-delay 0.1
+  (setq company-idle-delay 0.2
         company-minimum-prefix-length 1
-        company-selection-wrap-around t
         company-tooltip-align-annotations t
         company-tooltip-limit 20
-        company-show-numbers t
-        company-transformers '(company-sort-by-statistics))
+        company-show-numbers t)
 
-  ;; Key bindings for company
+  ;; Key bindings
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (define-key company-active-map (kbd "C-d") 'company-show-doc-buffer)
-  (define-key company-active-map (kbd "M-.") 'company-show-location)
+  (define-key company-active-map (kbd "RET") 'company-complete-selection)
+
   (global-company-mode))
 
+;; Yasnippet for code templates
+(use-package yasnippet
+  :commands yas-global-mode
+  :config
+  (yas-global-mode 1))
 
 ;; Flycheck for syntax checking
 (use-package flycheck
   :commands flycheck-mode
   :config
-  (setq flycheck-emacs-lisp-load-path 'inherit)
   (global-flycheck-mode))
 
-;; LSP Treemacs integration
-(use-package lsp-treemacs
-  :commands lsp-treemacs-errors-list
-  :after (lsp-mode treemacs))
-
-;; Projectile integration with LSP
-(defun my-lsp-projectile-find-file ()
-  "Find file in project with LSP support."
-  (interactive)
-  (call-interactively 'projectile-find-file))
-
-;; Auto-enable LSP for programming modes
-(dolist (hook '(java-mode-hook
-                python-mode-hook
-                javascript-mode-hook
-                typescript-mode-hook
-                js-mode-hook
-                web-mode-hook
-                css-mode-hook
-                go-mode-hook
-                rust-mode-hook
-                c-mode-hook
-                c++-mode-hook))
-  (add-hook hook (lambda ()
-                   (when (locate-dominating-file default-directory
-                                                 (lambda (dir)
-                                                   (or (file-exists-p (expand-file-name "pom.xml" dir))
-                                                       (file-exists-p (expand-file-name "build.gradle" dir))
-                                                       (file-exists-p (expand-file-name "package.json" dir))
-                                                       (file-exists-p (expand-file-name "Cargo.toml" dir))
-                                                       (file-exists-p (expand-file-name "go.mod" dir)))))
-                     (lsp-deferred)))))
+;; Auto-enable LSP for Java
+(add-hook 'java-mode-hook
+          (lambda ()
+            (when (fboundp 'lsp-deferred)
+              (lsp-deferred))))
 
 (provide 'init-lsp)
